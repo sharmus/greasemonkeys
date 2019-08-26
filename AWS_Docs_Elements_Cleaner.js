bd4ced173f5d82ea570719ebddc2f931b7523444
sharmus <45380175+sharmus@users.noreply.github.com>
2019-08-26 13:52:47 +1000

    Update AWS_Docs_Elements_Cleaner.js

--- AWS_Docs_Elements_Cleaner.js
+++ AWS_Docs_Elements_Cleaner.js
@@ -5,7 +5,7 @@
 // @version      0.1
 // @description  Remove unwanted footer and other elements from AWS Docs pages
 // @author       Shariq Mustaquim
-// @match        https://www.techradar.com/news/internet/the-beginner-s-guide-to-greasemonkey-scripting-598247
+// @match        https://docs.aws.amazon.com/*
 // @grant        none
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
