8a344405e0abfff5d74adb0ad86417d8a6862403
sharmus <45380175+sharmus@users.noreply.github.com>
2019-11-15 20:41:33 +1100

    Update AWS_Docs_Elements_Cleaner.js

--- AWS_Docs_Elements_Cleaner.js
+++ AWS_Docs_Elements_Cleaner.js
@@ -8,15 +8,34 @@
 // @match        https://docs.aws.amazon.com/*
 // @grant        none
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
-    'use strict';
+   'use strict';
+
+    //console.error("Starting...");
+
+    waitForKeyElements ('#awsdocs-footer-row > div:nth-child(2) > div.awsui-util-mb-m.ng-scope > div:nth-child(1) > div.next-link.awsui-util-font-size-3.ng-scope > a', actionFunction1);
+
+    function actionFunction1 (jNode) {
+        jNode.append(" >>>>>>>>>>>>>>>>>>>>>>>> ");
+    }
+
+    waitForKeyElements ('body > div > awsdocs-cookie-banner', actionFunction2);
+
+    function actionFunction2 (jNode) {
+        jNode.remove();
+    }
+
+    waitForKeyElements ('#awsdocs-header > div.ng-scope > awsui-flash > div', actionFunction3);
+
+    function actionFunction3 (jNode) {
+        jNode.remove();
+    }
 
     $("#footer").remove();
     $("#cookie-notice").remove();
     $("#awsdocs-cookie-banner").remove();
-    $('#awsdocs-header > div.ng-scope > awsui-flash > div').remove();
-    $('body > div > awsdocs-cookie-banner').remove();
 
 })();
